54b016c125c05074251d70e4d7828e931482ea42
nicolas barra <nicolasbarrapelecano@gmail.com>
2021-10-23 20:41:45 -0300

    persistencia en local storage del ultimo color

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const color = document.querySelector('#color');
 const valSalida = document.querySelector("#salida")
-color.addEventListener("input", () => { 
-    valSalida.innerHTML = color.value;
-    valSalida.style.backgroundColor = color.value;
-    valSalida.style.color = "#ffffff";
-});
+const colorStorage = localStorage.getItem('localColor') || "#ffffff";
+color.value = colorStorage;
+
+const setColor = () => {
+    const valColor = color.value;
+    valSalida.innerHTML = valColor;
+    valSalida.style.backgroundColor = valColor;
+}
+
+const saveColor = (valColor) => {
+    localStorage.setItem("localColor",valColor);
+}
+
+setColor();
+color.addEventListener("input", setColor);
+color.addEventListener ("change",()=>saveColor(color.value));
